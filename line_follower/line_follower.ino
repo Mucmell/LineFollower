ec73f2ddcdc45a700ce65ba1f240f7a7edd2caed
Mücahit ARDUÇ <arducmucahit@gmail.com>
2016-04-20 23:17:48 +0300

    basic logical control

--- line_follower/line_follower.ino
+++ line_follower/line_follower.ino
@@ -11,11 +11,52 @@ void setup() {
   pinMode(LEFT_PIN, INPUT);
   pinMode(RIGHT_PIN, INPUT);
   pinMode(MIDDLE_PIN, INPUT);
-}
-
-void loop() {
   leftMotor.run(FORWARD);
   rightMotor.run(FORWARD);
   leftMotor.setSpeed(150);
+  rightMotor.setSpeed(150);
+}
 
+void loop() {
+  int l = digitalRead(LEFT_PIN);
+  int r = digitalRead(RIGHT_PIN);
+  int m = digitalRead(MIDDLE_PIN);
+  if(m==1&&l==0&&r==0)
+  {}
+  else if(m==1&&l==1&&r==0)
+  {
+    rightMotor.faster(10);
+    leftMotor.slower(10);
+  }
+  else if(m==1&&l==0&&r==1)
+  {
+    rightMotor.slower(10);
+    leftMotor.faster(10);
+  }
+  else if(m==1&&l==1&&r==1)
+  {
+    leftMotor.setSpeed(150);
+    rightMotor.setSpeed(150);
+  }
+  else if(m==0&&l==0&&r==0)
+  {
+    leftMotor.setSpeed(150);
+    rightMotor.setSpeed(150);
+  }
+  else if(m==0&&l==1&&r==0)
+  {
+    rightMotor.faster(10);
+    leftMotor.slower(10);
+  }
+  else if(m==0&&l==0&&r==1)
+  {
+    rightMotor.slower(10);
+    leftMotor.faster(10);
+  }
+  else if(m==0&&l==1&&r==1)
+  {
+    leftMotor.setSpeed(150);
+    rightMotor.setSpeed(150);
+  }
+  delay(100);
 }
